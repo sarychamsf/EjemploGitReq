65e40a9d146cceba644b5924865180fe3840f093
Carlos Daniel Bayona <carlosdanielbayo@gmail.com>
2019-03-14 10:22:29 -0500

    missing spaces

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-  apiKey: 'AIzaSyAWUDX2eUpEQ4ZT6IezFInqM9Gi9tXDSqY',
-  authDomain: 'trazabilidadvino.firebaseapp.com',
-  databaseURL: 'https://trazabilidadvino.firebaseio.com',
-  projectId: 'trazabilidadvino',
-  storageBucket: 'trazabilidadvino.appspot.com',
-  messagingSenderId: '490748893236'
+    apiKey: 'AIzaSyAWUDX2eUpEQ4ZT6IezFInqM9Gi9tXDSqY',
+    authDomain: 'trazabilidadvino.firebaseapp.com',
+    databaseURL: 'https://trazabilidadvino.firebaseio.com',
+    projectId: 'trazabilidadvino',
+    storageBucket: 'trazabilidadvino.appspot.com',
+    messagingSenderId: '490748893236'
   }
-  };
+};
