b51d40b54a7a535bf6d175d6722acac08976a076
AndreaRuizNaranjo <andrea.ruiz.naranjo@gmail.com>
2019-03-28 15:46:51 -0500

    Back-end produccion-vino

--- src/app/produccion-vino/produccion-vino.component.ts
+++ src/app/produccion-vino/produccion-vino.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore , AngularFirestoreCollection} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface ProduccionVino {
+   IngredientesMezcla: string;
+   ProcesoProduccion: string;
+}
 
 @Component({
   selector: 'app-produccion-vino',
   templateUrl: './produccion-vino.component.html',
   styleUrls: ['./produccion-vino.component.css']
 })
-export class ProduccionVinoComponent implements OnInit {
 
-  constructor() { }
+export class ProduccionVinoComponent  {
 
-  ngOnInit() {
+  private db: AngularFirestore;
+  userCollectionRef: AngularFirestoreCollection<ProduccionVino>;
+  items: ProduccionVino[];
+
+  constructor(db: AngularFirestore) { 
+  this.db = db;
+  this.userCollectionRef = db.collection<ProduccionVino>('produccion-vino');
+ 
+  this.userCollectionRef.snapshotChanges().subscribe( data =>{
+  if (data) {
+    this.items = data.map( item =>{
+      const data = item.payload.doc.data() as ProduccionVino;
+      data.IngredientesMezcla= item.payload.doc.id;
+      return data;
+    });
   }
+ }, 
+ err => console.log('Error ' + err),
+ () => console.log('yay'))
+
+}
+
+createProduccionVino(  IngredientesMezcla: string, ProcesoProduccion: string){
+return this.db.collection('produccion-vino').add({
+  IngredientesMezcla: IngredientesMezcla, 
+  ProcesoProduccion: ProcesoProduccion
+});
+}
+ 
 
 }
