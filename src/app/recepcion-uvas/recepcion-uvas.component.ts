46799a8f957a162deaaae0771d87d633f280049c
AndreaRuizNaranjo <andrea.ruiz.naranjo@gmail.com>
2019-03-28 16:16:52 -0500

    Back-end recepcion-uvas y back-end recepcion-uvas-bodega

--- src/app/recepcion-uvas/recepcion-uvas.component.ts
+++ src/app/recepcion-uvas/recepcion-uvas.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore , AngularFirestoreCollection} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface RecepcionUvas {
+  IdProveedor:string;
+  DetallesProductos:string;
+  RecepcionUva:Date;
+}
 
 @Component({
   selector: 'app-recepcion-uvas',
   templateUrl: './recepcion-uvas.component.html',
   styleUrls: ['./recepcion-uvas.component.css']
 })
-export class RecepcionUvasComponent implements OnInit {
+export class RecepcionUvasComponent {
 
-  constructor() { }
+  private db: AngularFirestore;
+  userCollectionRef: AngularFirestoreCollection<RecepcionUvas>;
+  items: RecepcionUvas[];
 
-  ngOnInit() {
+  constructor(db: AngularFirestore) { 
+  this.db = db;
+  this.userCollectionRef = db.collection<RecepcionUvas>('recepcion-uvas');
+ 
+  this.userCollectionRef.snapshotChanges().subscribe( data =>{
+  if (data) {
+    this.items = data.map( item =>{
+      const data = item.payload.doc.data() as RecepcionUvas;
+      data.IdProveedor= item.payload.doc.id;
+      return data;
+    });
   }
+ }, 
+ err => console.log('Error ' + err),
+ () => console.log('yay'))
 
+}
+
+createRecepcionVino(IdProveedor: string, DetallesProductos: string, RecepcionUva:Date){
+return this.db.collection('recepcion-uvas').add({
+IdProveedor: IdProveedor,
+DetallesProductos: DetallesProductos,
+RecepcionUva:RecepcionUva
+});
+}
 }
